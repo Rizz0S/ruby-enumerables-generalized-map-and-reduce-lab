738ee77442c5aeebfdc8e7cb8cddedc3db2ac905
Summer Rizzo <srainrizzo@gmail.com>
2019-12-07 21:38:08 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+
+def map (source)
+  result = []
+  index = 0
+  while index < source.count do
+    result.push(yield(source[index]))
+    index +=1
+  end
+
+  result
+end
+
+def reduce (source, start = nil)
+  if start
+    result = start
+    index = 0
+  else
+    result = source[0]
+    index = 1
+  end
+    while index < source.count do
+      result = yield(result, source[index])
+      index += 1
+    end
+  result
+end
