df23f74a71d4cae990afd55a502143ae1df68f1b
Summer Rizzo <srainrizzo@gmail.com>
2019-12-07 21:25:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,5 +12,11 @@ def map (source)
 end
 
 def reduce (source, start = 0)
-
+  index = 0
+  result = start
+    while index < source.count do
+      result += yield(source[index])
+      index += 1
+    end
+    result
 end
